1346a0c0d411f8c6fd0e172dd3b1fd00debe97ea
caren-achieng <caren.achieng@strathmore.edu>
2022-02-18 23:37:53 +0300

    New branch created

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
-//Route::get('login', [\App\Http\Controllers\LoginController::class, 'index'])->name('login');
 //Route::get('register', [\App\Http\Controllers\RegisterController::class, 'index'])->name('register');
 
 
